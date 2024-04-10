afa2202f6c3be1f2fefe775c237a2d8dacc61da5
Stefan Fischer <stefischer@mail.com>
2024-04-10 00:31:32 -0400

    Create default theme object

---
+++ src/themes/cute.ts
@@ -0,0 +1,20 @@
+import type { Theme, ThemeColors } from '@@types/theme.js';
+
+const colors: ThemeColors = {
+    primary: "#591C2C",
+    secondary: "#8C2E47",
+    accent: "",
+    background: "#260D14",
+    card: "#F25C84",
+    cardLight: "#F2CED8",
+    text: "#dedede",
+    muted: "#2f2a2c",
+    accentText: "#F26E3F"
+};
+
+const cute: Theme = {
+    name: "cute",
+    colors
+};
+
+export default cute;
