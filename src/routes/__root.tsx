5a7327986de7b8ee97b4aa2df1b406c6d94a9f7e
Stefan Fischer <stefischer@mail.com>
2024-04-10 00:33:58 -0400

    Create _root route component

---
+++ src/routes/__root.tsx
@@ -0,0 +1,32 @@
+import type { RootState } from '@@store/index';
+
+import React, { useEffect, useCallback } from 'react';
+import { createRootRoute, Outlet } from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+import { useSelector } from 'react-redux';
+
+import { theme as themeUtils } from '@@utils';
+
+export const Route = createRootRoute({
+    component: Root
+});
+
+function Root() {
+
+    const theme = useSelector((state: RootState) => state.theme);
+
+    const validateTheme = useCallback(() => {
+        themeUtils.applyTheme(theme);
+    }, [theme]);
+
+    useEffect(() => {
+        validateTheme();
+    }, [validateTheme, theme]);
+
+    return(
+        <React.Fragment>
+            <Outlet />
+            <TanStackRouterDevtools />
+        </React.Fragment>
+    );
+};
