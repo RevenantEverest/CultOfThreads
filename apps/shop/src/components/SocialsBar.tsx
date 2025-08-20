f8f57eb83d9271cd10d61e1e41e58b526c38635c
Stefan <stefischer@mail.com>
2025-08-20 10:57:31 -0400

    Create SocialsBar component

---
+++ apps/shop/src/components/SocialsBar.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { FaInstagram, FaFacebookF, FaTiktok, FaEtsy } from 'react-icons/fa6';
+import { SOCIAL_LINKS, SocialIcon } from '@repo/ui';
+
+function SocialsBar() {
+
+    return(
+        <div className="flex gap-10">
+            <SocialIcon icon={FaEtsy} tooltip="Facebook" to={SOCIAL_LINKS.ETSY.url} size="7xl" />
+            <SocialIcon icon={FaFacebookF} tooltip="Facebook" to={SOCIAL_LINKS.INSTAGRAM.url} size="7xl" />
+            <SocialIcon icon={FaInstagram} tooltip="Instagram" to={SOCIAL_LINKS.INSTAGRAM.url} size="7xl" />
+            <SocialIcon icon={FaTiktok} tooltip="Instagram" to={SOCIAL_LINKS.TIK_TOK.url} size="7xl" />
+        </div>
+    );
+};
+
+export default SocialsBar;
