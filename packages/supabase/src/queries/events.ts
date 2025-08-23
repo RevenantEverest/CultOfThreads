b8458e4f886a0a60488d759bfc8262a51b716a53
Stefan <stefischer@mail.com>
2025-08-22 22:57:21 -0400

    Create event supabase queries and types

---
+++ packages/supabase/src/queries/events.ts
@@ -0,0 +1,123 @@
+import { 
+    Event, 
+    EventWithMarket, 
+    CreateEventParams,
+    UpdateEventParams
+} from '../types/events.js';
+
+import { supabase } from '../supabaseClient.js';
+import { storage } from '../utils/index.js';
+
+export async function fetchAll(): Promise<EventWithMarket[]> {
+    const { data, error } = await (
+        supabase.from("events")
+        .select(`
+            *,
+            market:market_id (
+                *,
+                market_details (
+                    *
+                )
+            )
+        `)
+    );
+
+    if(error) {
+        throw error;
+    }
+
+    return data ?? [];
+};
+
+export async function fetchById(id: string): Promise<EventWithMarket> {
+
+    const { data, error } = await (
+        supabase.from("events")
+        .select(`
+            *,
+            market:market_id (
+                *,
+                market_details (
+                    *
+                )
+            )    
+        `)
+        .eq("id", id)
+    );
+
+    if(error) {
+        throw error;
+    }
+
+    if(!data[0]) {
+        throw new Error("Data is null");
+    }
+
+    return data[0];
+};
+
+export async function create(eventData: CreateEventParams): Promise<Event> {
+
+    const flyerFilePath = await storage.create("content", `events`, eventData.image);
+
+    const { data, error } = await (
+        supabase.from("events").insert({
+            market_id: eventData.market_id,
+            address: eventData.address,
+            date_from: eventData.date_from,
+            date_to: eventData.date_to,
+            flyer_url: flyerFilePath
+        }).select()
+    );
+
+    if(error) {
+        throw error;
+    }
+
+    if(!data[0]) {
+        throw new Error("Event is missing");
+    }
+
+    return data[0];
+};
+
+export async function update(eventData: UpdateEventParams): Promise<Event> {
+    if(eventData.image) {
+        await storage.destroy("content", eventData.flyer_url);
+        const eventImagePath = await storage.create("content", "events", eventData.image);
+        eventData.flyer_url = eventImagePath;
+    }
+
+    const { data, error } = await (
+        supabase.from("events").update({
+            address: eventData.address,
+            date_from: eventData.date_from,
+            date_to: eventData.date_to,
+            flyer_url: eventData.flyer_url
+        }).eq("id", eventData.id).select()
+    );
+
+    if(error) {
+        throw error;
+    }
+
+    if(!data[0]) {
+        throw new Error("Event is missing");
+    }
+
+    return data[0];
+};
+
+export async function destroy(event: EventWithMarket) {
+    await storage.destroy("content", event.flyer_url);
+
+    const { data, error } = await (
+        supabase.from("events").delete().eq("id", event.id)
+    );
+
+    if(error) {
+        throw error;
+    }
+
+    return data;
+};
