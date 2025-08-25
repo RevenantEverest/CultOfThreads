998e3983ef515fa6cbe273c657542c71561ffe2c
Stefan <stefischer@mail.com>
2025-08-24 20:53:43 -0400

    Add new queries and types to supabase package export

--- packages/supabase/src/index.ts
+++ packages/supabase/src/index.ts
@@ -2,8 +2,20 @@ export * from './database.types.js';
 export * from './supabaseClient.js';
 
 /* Queries */
-export * as productApi from './queries/products.js';
-export * from './types/products.js';
+export * as categoryApi from './queries/categories.js';
+export * from './types/categories.js';
+
+export * as eventsApi from './queries/events.js';
+export * from './types/events.js';
+
+export * as marketDetailsApi from './queries/marketDetails.js';
+export * from './types/marketDetails.js';
+
+export * as marketApi from './queries/markets.js';
+export * from './types/markets.js';
+
+export * as productCategoryApi from './queries/productCategories.js';
+export * from './types/productCategories.js';
 
 export * as productDetailsApi from './queries/productDetails.js';
 export * from './types/productDetails.js';
@@ -11,11 +23,13 @@ export * from './types/productDetails.js';
 export * as productMediaApi from './queries/productMedia.js';
 export * from './types/productMedia.js';
 
-export * as marketApi from './queries/markets.js';
-export * from './types/markets.js';
+export * as productApi from './queries/products.js';
+export * from './types/products.js';
 
-export * as marketDetailsApi from './queries/marketDetails.js';
-export * from './types/marketDetails.js';
+export * as productTagApi from './queries/productTags.js';
+export * from './types/productTags.js';
 
-export * as eventsApi from './queries/events.js';
-export * from './types/events.js';
+
+
+
+
