ac26dac20e59c4cc684d92d1de642042b429d9f2
Stefan <stefischer@mail.com>
2025-09-05 18:52:19 -0400

    Add UpcomingEvents, BestSellers and NewArrivals to home page

--- apps/shop/src/app/page.tsx
+++ apps/shop/src/app/page.tsx
@@ -1,13 +1,21 @@
+import type { Metadata, Viewport } from 'next';
+
 import React from 'react';
-import { Metadata } from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
+
 import { Button } from '@repo/ui';
 import { PageHeader, Layout } from '@@shop/components/Common';
-import { Highlights } from '@@shop/components/Homepage';
 import { FAQ } from '@@shop/components/FAQ';
+import { UpcomingEvents } from '@@shop/components/Events';
 import Newsletter from '@@shop/components/Newsletter';
 
 import { IMAGE_RESOURCES } from '@repo/ui';
-import Image from 'next/image';
+import { BestSellers, NewArrivals } from '@@shop/components/Products';
+
+export const viewport: Viewport = {
+    themeColor: "#FB5377"
+};
 
 export async function generateMetadata(): Promise<Metadata> {
     return {
@@ -29,25 +37,31 @@ function Home() {
     return (
         <React.Fragment>
             <PageHeader>
-                <div className="flex sm:flex-col md:flex-row md:gap-5 items-center justify-center md:px-72 h-[90dvh]">
+                <div className="flex flex-col md:flex-row md:gap-5 items-center justify-center md:px-72 h-[90dvh]">
                     <div className="order-2 md:order-1 md:flex-1 flex flex-col text-center md:text-left items-center md:items-start justify-center">
                         <h1 className="text-4xl md:text-6xl text-text font-bold mb-4">Horror Plushies</h1>
                         <p className="text-md text-text/60 font-semibold mb-5 md:mb-10 w-11/12 md:w-4/6">
                             Where Cuteness Meets the Creeps — Discover handmade horror plushies that blend spooky 
                             vibes with an irresistibly adorable twist. Perfect for collectors, horror fans, and anyone who loves the strange and sweet.
                         </p>
-                        <Button size="xxl" className="rounded-full text-white bg-primary">
-                            Shop Now
-                        </Button>
+                        <Link href="/shop">
+                            <Button size="xxl" className="rounded-full text-white bg-primary">
+                                Shop Now
+                            </Button>
+                        </Link>
                     </div>
                     <div className="order-1 md:order-2 md:flex-1 flex items-center justify-center">
                         <Image width={800} height={800} className="w-full" src={IMAGE_RESOURCES.LOGO_LANDSCAPE} alt="logo" />
                     </div>
                 </div>
             </PageHeader>
-            <Layout main transparent className="lg:px-96 !pt-4 pb-10 md:pb-20 gap-10 md:gap-50">
-                <Highlights />
-                <FAQ />
+            <Layout main transparent className="!pt-4 pb-10 md:pb-20 gap-50 md:gap-20">
+                <div className="flex flex-col gap-30">
+                    <NewArrivals />
+                    <BestSellers />
+                </div>
+                <UpcomingEvents />
+                <FAQ className="md:!w-10/12" />
             </Layout>
             <Newsletter className="w-full bg-card z-20 relative py-20 md:px-56" />
         </React.Fragment>
