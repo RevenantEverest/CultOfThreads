fdb6f759b89181f4b8c8f7cbe2d48ad55fc26032
Stefan <stefischer@mail.com>
2025-09-05 18:46:59 -0400

    Add functionality to Newsletter

--- apps/shop/src/components/Newsletter.tsx
+++ apps/shop/src/components/Newsletter.tsx
@@ -1,12 +1,51 @@
+"use client"
+
+import type { NewsletterFormValues } from './Forms/NewsletterForm';
+
 import React from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'react-hot-toast';
+
+import { ToastError, ToastSuccess } from '@repo/ui';
 import NewsletterForm from './Forms/NewsletterForm';
+
+import { newsletterApi } from '@repo/supabase';
+
+function isErrorWithMessage(error: unknown): error is { message: string } {
+    return (
+        typeof error === 'object' &&
+        error !== null &&
+        'message' in error &&
+        typeof (error as { message: unknown }).message === 'string'
+    );
+};
 
 function Newsletter({ className }: React.HTMLAttributes<HTMLDivElement>) {
 
+    const mutation = useMutation({
+        mutationFn: newsletterApi.create
+    });
+
+    const onSubmit = async (values: NewsletterFormValues) => {
+        try {
+            await mutation.mutateAsync({ email: values.email, first_name: values.first_name });
+        
+            toast((t) => (
+                <ToastSuccess toast={t} message={"Newsletter signup successful!"} />
+            ));
+        }
+        catch(error) {
+            console.error(error);
+            const errorMessage = isErrorWithMessage(error) ? error.message : null;
+            toast((t) => (
+                <ToastError toast={t} message={errorMessage ?? "Error signing up for Newsletter"} />
+            ));
+        }
+    };
 
     return(
         <div className={`${className}`}>
-            <NewsletterForm />
+            <NewsletterForm onSubmit={onSubmit} />
         </div>
     );
 };
