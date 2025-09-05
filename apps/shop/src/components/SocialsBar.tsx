fac700104b2b8ce7a7b0db7e4026c24b067492fd
Stefan <stefischer@mail.com>
2025-09-05 18:47:24 -0400

    Fix incorrect tooltips in SocialBar

--- apps/shop/src/components/SocialsBar.tsx
+++ apps/shop/src/components/SocialsBar.tsx
@@ -7,10 +7,10 @@ function SocialsBar() {
 
     return(
         <div className="flex gap-10">
-            <SocialIcon icon={FaEtsy} tooltip="Facebook" to={SOCIAL_LINKS.ETSY.url} size="7xl" />
+            <SocialIcon icon={FaEtsy} tooltip="Etsy" to={SOCIAL_LINKS.ETSY.url} size="7xl" />
             <SocialIcon icon={FaFacebookF} tooltip="Facebook" to={SOCIAL_LINKS.INSTAGRAM.url} size="7xl" />
             <SocialIcon icon={FaInstagram} tooltip="Instagram" to={SOCIAL_LINKS.INSTAGRAM.url} size="7xl" />
-            <SocialIcon icon={FaTiktok} tooltip="Instagram" to={SOCIAL_LINKS.TIK_TOK.url} size="7xl" />
+            <SocialIcon icon={FaTiktok} tooltip="TikTok" to={SOCIAL_LINKS.TIK_TOK.url} size="7xl" />
         </div>
     );
 };
