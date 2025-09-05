0450db8740b729c3850e905f3884f7adc2642dc6
Stefan <stefischer@mail.com>
2025-09-05 18:53:47 -0400

    Add meta tags and ContactContainer to Contact page

--- apps/shop/src/app/contact/page.tsx
+++ apps/shop/src/app/contact/page.tsx
@@ -1,11 +1,35 @@
+import type { Metadata, Viewport } from 'next';
+
 import React from 'react';
 import { PageHeader, Layout } from '@@shop/components/Common';
-import Newsletter from '@@shop/components/Newsletter';
-import ContactForm from '@@shop/components/Forms/ContactForm';
 import { FaEnvelope } from 'react-icons/fa6';
-import { FAQ } from '@@shop/components/FAQ';
 
+import { ContactContainer } from '@@shop/containers';
+import { FAQ } from '@@shop/components/FAQ';
+import Newsletter from '@@shop/components/Newsletter';
 import SocialsBar from '@@shop/components/SocialsBar';
+
+import { IMAGE_RESOURCES } from '@repo/ui';
+
+export const viewport: Viewport = {
+    themeColor: "#FB5377"
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        title: "Cult of Threads | Contact",
+        description: `
+            Discover unique handmade crochet goods and creepy plush horror characters. Explore our collection for quirky gifts, spooky décor, and one-of-a-kind creations
+        `,
+        openGraph: {
+            siteName: "Cult of Threads | Handmade Crochet Plushies",
+            images: IMAGE_RESOURCES.OPEN_GRAPH
+        },
+        twitter: {
+            images: IMAGE_RESOURCES.TWITTER_META
+        }
+    };
+};
 
 function Contact() {
 
@@ -25,7 +49,7 @@ function Contact() {
             <Layout main transparent className="gap-50 mb-40">
                 <div className="flex flex-col w-full items-center justify-center gap-10">
                     <h1 className="text-5xl font-bold font-beach">Contact Us</h1>
-                    <ContactForm />
+                    <ContactContainer />
                 </div>
                 <SocialsBar />
                 <div className="bg-accent/50 rounded-full h-1 w-full" />
