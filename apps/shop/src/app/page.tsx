923c702e9d2a4ee677b40ea14481501de55544f1
Stefan <stefischer@mail.com>
2025-09-06 05:34:15 -0400

    Fix mobile styling, and wording on homepage header

--- apps/shop/src/app/page.tsx
+++ apps/shop/src/app/page.tsx
@@ -37,12 +37,14 @@ function Home() {
     return (
         <React.Fragment>
             <PageHeader>
-                <div className="flex flex-col md:flex-row md:gap-5 items-center justify-center md:px-72 h-[90dvh]">
+                <div className="flex flex-col md:flex-row md:gap-5 items-center justify-center md:px-72 h-[90dvh] relative -top-5">
                     <div className="order-2 md:order-1 md:flex-1 flex flex-col text-center md:text-left items-center md:items-start justify-center">
-                        <h1 className="text-4xl md:text-6xl text-text font-bold mb-4">Handmade Crochet Horror Plushies</h1>
-                        <p className="text-md text-text/60 font-semibold mb-5 md:mb-10 w-11/12 md:w-5/6">
-                            Welcome to the Cult of Threads! We make unique crochet plushies that are creepy, cute, and full of personality. 
-                            If you&apos;re into horror or just love things that are a little weird, you&apos;ve found your people. Find your new spooky friend today!
+                        <h1 className="text-3xl md:text-6xl text-text font-bold mb-4 px-5 md:px-0">
+                            Handmade Crochet <span className="bg-gradient-to-tr from-primary to-accent bg-clip-text text-transparent">Horror</span> Plushies
+                        </h1>
+                        <p className="text-md text-text/60 font-semibold mb-5 md:mb-10 w-10/12 md:w-5/6">
+                            Welcome to Cult of Threads! We make unique crochet plushies that are creepy, cute, and full of personality. 
+                            If you&apos;re into horror or just love things that are a little weird, you&apos;ve found your people.
                         </p>
                         <Link href="/shop">
                             <Button size="xl" className="rounded-full text-white bg-primary">
@@ -51,7 +53,15 @@ function Home() {
                         </Link>
                     </div>
                     <div className="order-1 md:order-2 md:flex-1 flex items-center justify-center">
-                        <Image width={800} height={800} className="w-full" src={IMAGE_RESOURCES.LOGO_LANDSCAPE} alt="logo" />
+                        <div className="w-60 h-50 lg:w-140 lg:h-120 flex overflow-hidden">
+                            <Image 
+                                width={800} 
+                                height={800} 
+                                className="shrink-0 relative object-cover w-full h-full rounded-lg" 
+                                src={IMAGE_RESOURCES.LOGO_LANDSCAPE} 
+                                alt="logo"
+                            />
+                        </div>
                     </div>
                     <ScrollToSeeMore className="hidden md:block" />
                 </div>
