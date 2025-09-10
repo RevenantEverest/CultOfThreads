5c5b86152f788981e01a9e6ed28e32009ea341b3
Stefan <stefischer@mail.com>
2025-09-10 05:01:33 -0400

    Update AddContact button

--- apps/admin/src/components/Contacts/AddContact.tsx
+++ apps/admin/src/components/Contacts/AddContact.tsx
@@ -1,15 +1,16 @@
+import { Link } from '@tanstack/react-router';
 import { Button } from '@repo/ui';
 
 import { FaPlus } from 'react-icons/fa6';
 
 function AddContact() {
     return(
-        <div>
-            <Button className="!font-semibold !text-sm text-text bg-card-light hover:bg-card-light">
+        <Link to="/dashboard/contacts/add">
+            <Button className="!font-semibold !text-sm text-white bg-card-light hover:bg-card-light">
                 <FaPlus />
                 Add Contact
             </Button>
-        </div>
+        </Link>
     );
 };
 
