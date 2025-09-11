898946bc4ddcd3952611624ead63002fd83a8cb7
Stefan <stefischer@mail.com>
2025-09-11 15:52:35 -0400

    Revert custom supabase image loader

--- apps/shop/next.config.ts
+++ apps/shop/next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     images: {
-        loader: "custom",
-        loaderFile: "./supabaseLoader.js",
         remotePatterns: [
             {
                 protocol: "https",
--- apps/shop/supabaseLoader.js
+++
@@ -1,7 +0,0 @@
-import process from 'node:process';
-
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-
-export default function supabaseLoader({ src, width, quality }) {
-    return `${SUPABASE_URL}${src}?width=${width}&quality=${quality}`;
-};
