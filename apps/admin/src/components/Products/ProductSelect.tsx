3f794c3902361ae238eaa79193ac99463e3ba9c4
Stefan <stefischer@mail.com>
2025-09-25 13:59:33 -0400

    Update map key and styling in ProductSelect

--- apps/admin/src/components/Products/ProductSelect.tsx
+++ apps/admin/src/components/Products/ProductSelect.tsx
@@ -17,21 +17,21 @@ interface ProductSelectProps {
 function ProductSelect({ value, products, onChange }: ProductSelectProps) {
 
     const renderMarkets = () => {
-        return products.sort((a, b) => a.name.localeCompare(b.name)).map((product) => (
-            <SelectItem key={`product-select-${product.name}`} value={product.id}>
+        return products.sort((a, b) => a.name.localeCompare(b.name)).map((product, index) => (
+            <SelectItem key={`product-select-${product.name}-${index}`} value={product.id}>
                 {product.name}
             </SelectItem>
         ));
     };
 
     return(
-        <div>
+        <div className="flex flex-col">
             <p className="text-sm font-bold mb-1.5">Product</p>
             <Select 
                 value={value ?? undefined} 
                 onValueChange={(value) => onChange(value)}
             >
-                <SelectTrigger className="bg-card-light px-2.5 py-2.5 rounded-md font-semibold text-sm w-full">
+                <SelectTrigger className="bg-card-light px-2.5 py-5 rounded-md font-semibold text-sm w-full">
                     <SelectValue placeholder="Choose A Product" />
                 </SelectTrigger>
                 <SelectContent className="font-semibold">
