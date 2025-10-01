8534119b847ee9689b5d7a2a3c6ba882cbeded22
Stefan <stefischer@mail.com>
2025-09-30 20:20:43 -0400

    Create ProductCategories component

--- apps/shop/src/components/Products/index.tsx
+++ apps/shop/src/components/Products/index.tsx
@@ -1,6 +1,7 @@
 export { default as BestSellers } from './BestSellers';
 export { default as NewArrivals } from './NewArrivals';
 export { default as Product } from './Product';
+export { default as ProductCategories } from './ProductCategories';
 export { default as ProductSort } from './ProductSort';
 
 export { default as ProductList } from './ProductList';
