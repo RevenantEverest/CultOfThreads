6e92169b1288c98577e8a2079e86dd14095fe440
Stefan <stefischer@mail.com>
2025-09-30 20:20:03 -0400

    Create Breadcrumb component in shop app

--- apps/shop/src/components/Common/index.tsx
+++ apps/shop/src/components/Common/index.tsx
@@ -1,3 +1,4 @@
+export { default as Breadcrumb } from './Breadcrumb';
 export { default as Lightbox } from './Lightbox';
 export { default as Layout } from './Layout';
 export { default as PageHeader } from './PageHeader';
