30c79cdf565e5d5840d922654dc47bbbca026454
Stefan <stefischer@mail.com>
2026-07-30 06:37:55 -0400

    Update api environment constants

--- apps/api/src/constants/environment.ts
+++ apps/api/src/constants/environment.ts
@@ -14,8 +14,9 @@ export const SUPABASE_PROJECT_ID = process.env.SUPABASE_PROJECT_ID as string;
 export const SUPABASE_DB_PASSWORD = process.env.SUPABASE_DB_PASSWORD as string;
 export const SUPABASE_AUTH_URL = `${SUPABASE_URL}/auth/v1`;
 
+const hostRegex = /^https?:\/\/([^:/]+)/gi;
 export const DATABASE = {
-    HOST: "127.0.0.1",
+    HOST: hostRegex.exec(SUPABASE_URL)![1] as string,
     PORT: Number(process.env.DB_PORT as string),
     NAME: process.env.DB_NAME as string,
     USERNAME: process.env.DB_USERNAME as string,
