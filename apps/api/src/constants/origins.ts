ee9d38c003185e1aeb153281c08145d2ae094619
Stefan Fischer <33422278+RevenantEverest@users.noreply.github.com>
2026-07-30 08:40:00 -0400

    Update allowed origins array (#29)

--- apps/api/src/constants/origins.ts
+++ apps/api/src/constants/origins.ts
@@ -2,5 +2,7 @@ export const ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:4000",
     "http://cultofthreads.com",
-    "http://admin.cultofthreads.com"
+    "http://admin.cultofthreads.com",
+    "https://cultofthreads.com",
+    "https://admin.cultofthreads.com"
 ];
