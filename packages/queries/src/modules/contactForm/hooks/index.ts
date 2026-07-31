b71551b019f7fb7f5a11404606679b6935b848d7
Stefan <stefischer@mail.com>
2026-07-31 07:19:20 -0400

    Create update and destroy actions and hooks for contact form module in queries package

--- packages/queries/src/modules/contactForm/hooks/index.ts
+++ packages/queries/src/modules/contactForm/hooks/index.ts
@@ -1,3 +1,5 @@
 export * from './useCreateSubmission.hook';
+export * from './useDestroy.action';
 export * from './useIndex.hook';
 export * from './useGetOne.hook';
+export * from './useUpdate.hook';
